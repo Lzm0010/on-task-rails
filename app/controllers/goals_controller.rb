0dc517a52f4fcc542dd252f8a246a4451e140eb1
Correct Name lzm0010@gmail.com <github email address>
2020-06-04 20:21:29 -0500

    goals to nest tasks

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,13 +1,17 @@
 class GoalsController < ApplicationController
     def index
         goals = Goal.all
-        render json: goals
+        render json: goals.to_json(:include => {
+            :tasks => {:only => [:id, :name, :step_number, :project_id, :goal_id, :planner_id, :status, :is_completed, :date]}
+        })
     end
 
     def create
         goal = Goal.new(goal_params)
         if goal.save
-            render json: goal
+            render json: goal.to_json(:include => {
+                :tasks => {:only => [:id, :name, :step_number, :project_id, :goal_id, :planner_id, :status, :is_completed, :date]}
+            })
         else
             render json: {"message": "Goal couldn't be created."}
         end
@@ -16,7 +20,9 @@ class GoalsController < ApplicationController
     def update
         goal = Goal.find(params[:id])
         if goal.update_attributes(goal_params)
-            render json: goal
+            render json: goal.to_json(:include => {
+                :tasks => {:only => [:id, :name, :step_number, :project_id, :goal_id, :planner_id, :status, :is_completed, :date]}
+            })
         else
             render json: {"message": "Something went wrong!"}
         end
@@ -25,7 +31,9 @@ class GoalsController < ApplicationController
     def destroy
         goal = Goal.find(params[:id])
         if goal.destroy
-            render json: goal
+            render json: goal.to_json(:include => {
+                :tasks => {:only => [:id, :name, :step_number, :project_id, :goal_id, :planner_id, :status, :is_completed, :date]}
+            })
         else
             render json: {"message": "Couldn't delete goal."}
         end
@@ -34,7 +42,7 @@ class GoalsController < ApplicationController
     private
     
     def goal_params
-        params.require(:goal).permit(:name, :start_date, :end_date, :goal_type, :goal_total_days, :goal_percentage, :user_id, :frequency)
+        params.require(:goal).permit(:name, :start_date, :end_date, :goal_type, :goal_total_days, :goal_percentage, :user_id, :frequency, tasks_attributes: [:id, :name, :status, :is_completed, :date, :goal_id])
     end
     
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,5 @@
 class Goal < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :delete_all
+  accepts_nested_attributes_for :tasks, allow_destroy: true
 end
