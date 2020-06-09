a3540999bfc9f6b6c9f37c2f0b956b0be6778b70
Correct Name lzm0010@gmail.com <github email address>
2020-06-09 09:24:13 -0500

    added jwt and login functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,19 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+    
     def index
         users = User.all
         render json: users
     end
 
     def create
-        user = User.new(user_params)
-        if user.save
-            render json: user
+        user = User.create(user_params)
+        if user.valid?
+          payload = {user_id: user.id}
+          token = encode_token(payload)
+          render json: {user:user, jwt: token}
         else
-            render json: {"message": "user couldn't be created."}
+          render json: {errors: user.errors.full_messages}, status: :not_acceptable
         end
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
     has_many :notes, through: :planner
 
     has_secure_password
+    validates :username, uniqueness: {case_sensitive: false}
 end
